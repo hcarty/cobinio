05bc85316d54cf39fd2d454f4e732496093ebc76
Hezekiah M. Carty <hez@0ok.org>
2016-02-24 22:49:44 -0500

    Add some doc comments

--- src/cobinio.mli
+++ src/cobinio.mli
@@ -1,5 +1,7 @@
+(** {1 mmap-like IO} *)
+
 type 'elt kind =
-    Char : char kind
+  | Char : char kind
   | Int8_signed : int kind
   | Int8_unsigned : int kind
   | Int16_signed : int kind
@@ -8,16 +10,19 @@ type 'elt kind =
   | Int64 : int64 kind
   | Float32 : float kind
   | Float64 : float kind
+  (** Type of values to read/write *)
 
 type input = Lwt_io.input
 type output = Lwt_io.output
 type 'mode channel = 'mode Lwt_io.channel
 
 type ('elt, 'mode) t
+(** Sources with values of type ['elt] and ['mode] permissions. *)
 
 val little : (module EndianBytes.EndianBytesSig)
 val big : (module EndianBytes.EndianBytesSig)
 val native : (module EndianBytes.EndianBytesSig)
+(** Endianness of values read/written *)
 
 val make :
   'mode channel ->
@@ -25,6 +30,15 @@ val make :
   (module EndianBytes.EndianBytesSig) ->
   int ->
   ('elt, 'mode) t
+(** [make chan kind endian offset] wraps [chan] so that it is accessed as if it
+    is a contiguous sequence of [kind] elements.  [endian] indicates the
+    endianness of the data in [chan].
+
+    @param offset indicates how many bytes to skip at the start of the file, if
+    there's a header at the start of the file. *)
 
 val get : ('elt, input) t -> int -> 'elt Lwt.t
+(** [get src i] returns the [i]th value from [src]. *)
+
 val set : ('elt, output) t -> int -> 'elt -> unit Lwt.t
+(** [set src i v] sets the [i]th value in [src] to [v]. *)
